0bac6dcc3a6451a952e9cd4365dc72feefcdf986
Developer <{ID}+{username}@users.noreply.github.com>
2024-06-16 17:55:15 +0530

    backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const json_server=require('json-server')
+const cors=require('cors')
+
+const server=json_server.create()
+const middle=json_server.defaults()
+const db=json_server.router('db.json')
+
+
+server.use(middle)
+server.use(cors())
+server.use(db)
+
+const PORT=8000
+server.listen(PORT,()=>{
+    console.log(`Port running at ${PORT}`);
+})
